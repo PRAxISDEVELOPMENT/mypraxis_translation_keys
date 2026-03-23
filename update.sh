697052130165a54a5c951527faeb20e1d3f40c16
Geoffrey (PRAxIS) <geoffrey.santy@praxis-group.eu>
2026-03-23 12:45:45 +0100

    test

--- update.sh
+++ update.sh
@@ -11,5 +11,16 @@ fi
 git add .
 git commit -m "$MESSAGE"
 git push
-sleep 120
+
+SECONDS_LEFT=120
+
+while [ $SECONDS_LEFT -gt 0 ]; do
+  printf "\rPull in %02d seconden..." "$SECONDS_LEFT"
+  sleep 1
+  SECONDS_LEFT=$((SECONDS_LEFT - 1))
+done
+
+printf "\rPulling latest changes now...            \n"
 git pull --rebase origin main
+
+echo "Klaar."
